6f9b05725d779e95060758d483fe8789f2dd1132
Dennis Smith <dds5106@gmail.com>
2018-10-30 19:44:59 -0400

    Importing Project

---
+++ ConsoleAppGitAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Git...");
+            Console.ReadLine();
+        }
+    }
+}
